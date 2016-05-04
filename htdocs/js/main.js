26f34c65ad1e43241cad4d269bc8c24eb05fde8c
Kevin Justal <justal.kevin@gmail.com>
2016-05-04 11:06:08 +0700

    Modifying the value of the parallax of the two slides

--- htdocs/js/main.js
+++ htdocs/js/main.js
@@ -673,14 +673,39 @@ $(function() {
 		fixscroll();
 	});
 	
+	var checkScrollSpeed = (function(settings){
+	    settings = settings || {};
+
+	    var lastPos, newPos, timer, delta, 
+	        delay = settings.delay || 50; // in "ms" (higher means lower fidelity )
+
+	    function clear() {
+	      lastPos = null;
+	      delta = 0;
+	    }
+
+	    clear();
+
+	    return function(){
+	      newPos = window.scrollY;
+	      if ( lastPos != null ){ // && newPos < maxScroll 
+	        delta = newPos -  lastPos;
+	      }
+	      lastPos = newPos;
+	      clearTimeout(timer);
+	      timer = setTimeout(clear, delay);
+	      return delta;
+	    };
+	})();
+	
 	function parallax() {
 		if(positionTopScroll>anchorPosition[0]) {
-			var cal = (positionTopScroll/anchorPosition[0])*7;
-			$("#parallax").css("background-position","0% "+cal+"%");
+			var cal = -(positionTopScroll-anchorPosition[0])/5.5;
+			$("#parallax").css("background-position","0% "+cal+"px");
 		}
 		if(positionTopScroll>anchorPosition[2]) {
-			var cal = (positionTopScroll/anchorPosition[2])*45;
-			$("#temoignages-bg").css("background-position","0% "+cal+"%");
+			var cal = -(positionTopScroll-anchorPosition[2])/5.5;
+			$("#temoignages-bg").css("background-position","0% "+cal+"px");
 		}
 	}
 	
