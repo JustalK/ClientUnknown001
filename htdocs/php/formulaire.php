d808c32264a5a9a8b02e8213a395083370cd75e0
Kevin Justal <justal.kevin@gmail.com>
2016-05-19 09:48:59 +0700

    Modification du formulaire en ajoutant 2 champs inutiles et modifiant l'adresse de post

--- htdocs/php/formulaire.php
+++ htdocs/php/formulaire.php
@@ -6,7 +6,7 @@
  * ------------------------------------
  */
 
-$to    = "test@surjithctly.in"; // <--- Your email ID here
+$to    = "justal.kevin.spam@gmail.com"; // <--- Your email ID here
 
 
 /*
@@ -19,6 +19,8 @@ $name     = $_REQUEST["name"];
 $email    = $_REQUEST["email"];
 $subject  = $_REQUEST["subject"];
 $msg      = $_REQUEST["message"];
+$champ1      = $_REQUEST["champ1"];
+$champ2      = $_REQUEST["champ2"];
 
 if (isset($email) && isset($name)) {
 	 
@@ -26,7 +28,7 @@ if (isset($email) && isset($name)) {
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
 	$headers .= "From: ".$name." <".$email.">\r\n"."Reply-To: ".$email."\r\n" ;
-	$msg     = "Hello Admin, <br/> <br/> You've got a message from $name ($email)<br/><br/>Message: $msg <br><br> -- <br>This e-mail was sent from a contact form on $website";
+	$msg     = "Hello Admin, <br/> <br/> You've got a message from $name ($email)<br/><br/>Message: $msg <br><br> -- <br>This e-mail was sent from a contact form on $website<br><br>Ceci est le champ1 : $champ1<br><br>Ceci est le champ2 : $champ2";
 
 	$mail =  mail($to, $subject, $msg, $headers);
 	if($mail)
